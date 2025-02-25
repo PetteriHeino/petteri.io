88095a6d2e17a548bc6911958140fea86303a121
Petteri Heino <petteriheino@Petteri-MacBook-Pro.local>
2025-02-25 17:17:40 +0200

    fix: _config.yml

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740491405',resources: [
+    cacheName: 'chirpy-1740496482',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
