180debb5986405946f08fe8a4f3c502fcc89f723
Petteri Heino <petteriheino@Petteri-MacBook-Pro.local>
2025-02-25 19:54:44 +0200

    fix: jotain

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740503654',resources: [
+    cacheName: 'chirpy-1740506048',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
