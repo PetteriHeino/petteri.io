140f04471411ea9a918395c7262c2316542ac91f
Petteri Heino <petteriheino@Petteri-MacBook-Pro.local>
2025-08-06 16:53:06 +0300

    chore: mfa

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754477655',resources: [
+    cacheName: 'chirpy-1754488245',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
