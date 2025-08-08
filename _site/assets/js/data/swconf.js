6f558d37861017da898b4b2e09f9123fa276bbcb
Petteri Heino <petteriheino@Petteri-MacBook-Pro.local>
2025-08-08 10:18:43 +0300

    chore: sspr

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754572306',resources: [
+    cacheName: 'chirpy-1754637458',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
